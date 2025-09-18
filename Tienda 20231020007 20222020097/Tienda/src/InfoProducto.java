8a1ed821d2f20b0c03c0ce3ec14b4e19a023bf9f
Juan <150098716+Juan20301@users.noreply.github.com>
2025-09-18 13:20:12 -0500

    Update InfoProducto.java
    Eliminacion de metodos no usados

--- Tienda 20231020007 20222020097/Tienda/src/InfoProducto.java
+++ Tienda 20231020007 20222020097/Tienda/src/InfoProducto.java
@@ -20,24 +20,5 @@ public class InfoProducto {
     public String getCategoria() {
         return categoria;
     }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        InfoProducto producto = (InfoProducto) o;
-        return Integer.compare(producto.precio, precio) == 0 &&
-                nombre.equals(producto.nombre) &&
-                categoria.equals(producto.categoria);
-    }
-
-    @Override
-    public int hashCode() {
-        int result = nombre != null ? nombre.hashCode() : 0;
-        long temp = Double.doubleToLongBits(precio);
-        result = 31 * result + (int) (temp ^ (temp >>> 32));
-        result = 31 * result + (categoria != null ? categoria.hashCode() : 0);
-        return result;
-    }
-
 }
+
